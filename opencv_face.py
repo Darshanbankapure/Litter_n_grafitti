1fdb6178c82579b92db2575218bd199b0880c504
notpoojakulkarni <125811993+notpoojakulkarni@users.noreply.github.com>
2023-09-09 00:27:39 +0530

    Update opencv_face.py

--- opencv_face.py
+++ opencv_face.py
@@ -20,6 +20,8 @@ while True:
     # Draw rectangles around detected faces
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.putText(frame, "Face Detected", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
 
     # Display the frame with face detection
     cv2.imshow("Face Detection", frame)
